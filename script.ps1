46886a9e1be967672beb2b79a385d3f2b20a6f85
Ji Yong Seong <jiyongseong@hotmail.com>
2020-08-25 12:22:58 +0900

    update emoji

--- script.ps1
+++ script.ps1
@@ -1,5 +1,5 @@
 $items = (Invoke-WebRequest -Uri 'https://api.github.com/emojis').Content | ConvertFrom-Json | Get-Member -MemberType NoteProperty
 foreach($item in $items)
 {
-    [string]::Format(":{0}: ``:{0}:``", $item.Name.ToString()) 
+    [string]::Format(":{0}: ``:{0}:``  ", $item.Name.ToString()) 
 }
